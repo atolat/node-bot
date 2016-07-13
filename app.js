54060492fc5cb7ac746ba76395fa01851465f94f
atolat <atolat@uci.edu>
2016-07-13 09:03:31 -0500

    Testing

--- app.js
+++ app.js
@@ -8,11 +8,25 @@ var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
 
 //Setting up a waterfall of functions....!
-bot.dialog('/', [function (session) {
-        builder.Prompts.text(session, 'Hi! What is ypur name?');
-}
-            , function (session, results) {
-        session.send('Hello ' + results.response);
-            }
+bot.dialog('/', [
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
 
-            ]);
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
