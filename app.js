cffa5698d3513044af563981e88c642f022af3e8
atolat <atolat@uci.edu>
2016-07-13 16:41:35 -0500

    waterfall...

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ var connector = new builder.ConsoleConnector().listen();
 //The UniversalBot class implements all of logic to manage the bots’ conversations with users. You can bind the UniversalBot to a variety of channels using connectors.
 var bot = new builder.UniversalBot(connector);
 
+var intents = new builder.IntentDialog();
+
 //Setting up a waterfall of functions....!
-bot.dialog('/', [
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
     function (session, args, next) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
